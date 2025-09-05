f306166968225a841ac1ba29f2e7d4b9db16b5c6
ssyy3034 <113993646+ssyy3034@users.noreply.github.com>
2025-09-05 11:40:43 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if score >=90:
+    print("A")
+if score >=80 and score <90:
+    print("B")
+if score >=70 and score <80:
+    print("C")
+if score >=60 and score <70:
+    print("D")
+if score <60:
+    print("F")
