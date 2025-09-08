f294e27d05983a4c3b729d6b1654453c63d1b13a
ssyy3034 <113993646+ssyy3034@users.noreply.github.com>
2025-09-08 13:53:44 +0900

    [Gold V] Title: 하노이 탑, Time: 804 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,13 +1,13 @@
+N = int(input())
 
 def hanoi(n,x,y,z):
     if n == 1:
         print(x,y)
         return
-
-    hanoi(n -1,x,z,y)
+    hanoi(n-1,x,z,y)
     print(x,y)
-    hanoi(n - 1,z,y,x)
-N = int(input())
+    hanoi(n-1,z,y,x)
 print(2**N-1)
-if N<=20:
+if N <=20:
     hanoi(N,1,3,2)
+
