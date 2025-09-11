1e9a2f56746a25fe36fad4d2786a79f284129a8b
ssyy3034 <113993646+ssyy3034@users.noreply.github.com>
2025-09-11 11:09:54 +0900

    [Silver II] Title: 부분수열의 합, Time: 224 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,22 +1,19 @@
 N,S = map(int,input().split())
-num_list = list(map(int,input().split()))
 
+num_list = list(map(int,input().split()))
 count = 0
-test = []
-def find(idx,num,used):
+
+def calc(idx,hap,used):
     global count
 
     if idx == N:
-        if num == S and used != 0:
-            count +=1
+        if hap == S and used != 0:
+            count += 1
         return
 
-    find(idx+1,num+num_list[idx],used+1)
-    find(idx+1,num,used)
+    calc(idx+1,hap+num_list[idx],used+1)
 
-find(0,0,0)
+    calc(idx+1,hap,used)
+calc(0,0,0)
 print(count)
-
-
-
 
