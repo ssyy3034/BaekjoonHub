3413671cf993151422fc63e8af784f04aae92a8e
Gwon Dongha <113993646+ssyy3034@users.noreply.github.com>
2025-09-18 10:04:40 +0900

    [Silver V] Title: 숫자 카드, Time: 3732 ms, Memory: 109828 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,27 +2,24 @@ n = int(input())
 n_list = list(map(int, input().split()))
 n_list.sort()
 
-m_num = int(input())
+m = int(input())
 m_list = list(map(int, input().split()))
-
-
 answer = []
 for m in m_list:
-    found = False
     l = 0
     r = n-1
-    while l<=r:
+    found = False
+    while l <= r:
         mid = (l+r)//2
         if n_list[mid] == m:
             found = True
             break
-        if n_list[mid] < m:
+        elif n_list[mid] < m:
             l = mid+1
-        elif n_list[mid] > m:
+        else:
             r = mid-1
     if found:
         answer.append(1)
     else:
         answer.append(0)
-
 print(*answer)
