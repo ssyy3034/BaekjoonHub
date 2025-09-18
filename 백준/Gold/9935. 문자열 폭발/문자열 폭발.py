8a3fc4a27e29910396cb5c68e82af353eca07bff
Gwon Dongha <113993646+ssyy3034@users.noreply.github.com>
2025-09-18 10:48:51 +0900

    [Gold IV] Title: 문자열 폭발, Time: 492 ms, Memory: 190144 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,23 +1,19 @@
 import sys
-
-# 입력 문자열과 폭발 문자열의 양쪽 공백 제거
-N = sys.stdin.readline().strip()
+sentence = sys.stdin.readline().strip()
 bomb = sys.stdin.readline().strip()
 
-answer = []
 bomb_len = len(bomb)
 
-for char in N:
-    answer.append(char)
-    # IndexError를 방지하기 위해 스택의 길이가 충분한지 확인
-    if len(answer) >= bomb_len:
-        # 스택의 마지막 부분이 폭발 문자열과 일치하는지 확인
-        if ''.join(answer[-bomb_len:]) == bomb:
-            # 일치하면 폭발 문자열만큼 pop
-            for _ in range(bomb_len):
-                answer.pop()
+answer = []
+#answer에 담아가면서 만약 -bomb_len으로 뒤에서부터 슬라이싱 했을때 bomb이면 answer.pop()
 
-if not answer:
+for char in sentence:
+    answer.append(char)
+    if ''.join(answer[-bomb_len:]) == bomb:
+        for _ in range(bomb_len):
+            answer.pop()
+if len(answer) == 0:
     print("FRULA")
 else:
     print(''.join(answer))
+
