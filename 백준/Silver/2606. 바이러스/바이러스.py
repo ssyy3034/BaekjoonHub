b2ba14cec08cf9943b0cf37047b8624fb3eca529
Gwon Dongha <113993646+ssyy3034@users.noreply.github.com>
2025-09-24 13:43:10 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -2,24 +2,21 @@ from collections import deque
 
 n = int(input())
 m = int(input())
-
-graph = [[] for _ in range(n+1)]
-visited = [0 for _ in range(n+1)]
+graph = [[] for i in range(n+1)]
+visited = [0 for i in range(n+1)]
 for _ in range(m):
-    a,b = map(int, input().split())
+    a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-q = deque()
-q.append(1)
 
-while len(q) > 0:
-    v = q.popleft()
-    visited[v] = 1
-    for nxt in graph[v]:
-        if visited[nxt] == 1:
-            continue
-        q.append(nxt)
-
-print(sum(visited) - 1)
-
-
+def dfs(node):
+    visited[node] = 1
+    for nxt in graph[node]:
+        if visited[nxt] == 0:
+            dfs(nxt)
+dfs(1)
+count = 0
+for t in visited:
+    if t:
+        count+=1
+print(count-1)
