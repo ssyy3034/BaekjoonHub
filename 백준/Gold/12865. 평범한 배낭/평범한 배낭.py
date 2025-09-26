f6189f32fb608bd6d790491387fabc3e72ca5cf4
Gwon Dongha <113993646+ssyy3034@users.noreply.github.com>
2025-09-26 21:28:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 5168 ms, Memory: 230376 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,18 @@
+N, K = map(int, input().split())
+items = []  # (무게, 가치)
+for _ in range(N):
+    W, V = map(int, input().split())
+    items.append((W, V))
+
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    W, V = items[i - 1]
+    for k in range(K + 1):
+        
+        dp[i][k] = dp[i - 1][k]
+       
+        if k >= W:
+            dp[i][k] = max(dp[i][k], dp[i - 1][k - W] + V)
+
+print(dp[N][K])
